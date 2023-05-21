072c4f688efc3f0f01745a474d5c2cffb5616be2
helodeepakji <helodeepakji@gmail.com>
2023-05-21 07:46:43 +0530

    login system

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const bcrypt = require('bcrypt');
 const connection = require('./db/mysqlconn');
+const session = require('express-session');
+
 
 const app = express();
 const port = 3000;
@@ -13,10 +16,26 @@ const layoutPath = path.join(__dirname, "/view/layout");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.use(cookieParser());
+app.use(session({
+  secret: 'helodeepakji',
+  resave: false,
+  saveUninitialized: false,
+}));
+
 app.set('views', viewPath);
 app.set('view engine', 'hbs');
 app.use(express.static(staticPath));
 hbs.registerPartials(layoutPath);
+
+
+
+function requireAuth(req, res, next) {
+  if (!req.session.email) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  next();
+}
 
 
 // Route to render the HBS template
@@ -64,7 +83,8 @@ app.get('/signup',(req,res)=>{
 });
 
 app.post('/signup',(req,res)=>{
-  connection.query('INSERT into `login` (`name`,`email`,`password`) VALUES ("'+req.body.name+'","'+req.body.email+'","'+req.body.password+'")',(error, results, fields) => {
+  const hashedpassword = bcrypt.hashSync(req.body.password,10);
+  connection.query('INSERT into `login` (`name`,`email`,`password`) VALUES ("'+req.body.name+'","'+req.body.email+'","'+hashedpassword+'")',(error, results, fields) => {
     if (error) {
       console.log(error);
       res.render('signup',{status:"failed"});
@@ -79,8 +99,48 @@ app.get('/login',(req,res)=>{
   res.render('login');
 });
 
-app.post('/login',(req,res)=>{
-  res.render('login');
+// Route to handle login
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Your database query to fetch user by email
+  connection.query('SELECT * from `login` WHERE `email` = ?', [email], (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.render('signup', { status: 'failed' });
+    } else {
+      if (results.length > 0) {
+        const user = results[0];
+        const isPassword = bcrypt.compareSync(password, user.password);
+        if (isPassword) {
+          console.log(user);
+          console.log(isPassword);
+          req.session.email = user.email;
+          res.json({ message: 'login successs' });
+        } else {
+          res.render('login', { status: 'Enter correct password' });
+        }
+      } else {
+        res.render('signup', { status: 'please signup' });
+      }
+    }
+  });
+});
+
+
+app.get('/order', requireAuth, (req, res) => {
+  res.json({ message: 'Access granted to protected route' });
+});
+
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      throw err;
+    }
+    res.json({ message: 'Logout successful' });
+  });
 });
 
 
--- db/mysqlconn.js
+++ db/mysqlconn.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "paytm"
+    database: "node"
 });
 
 connection.connect(function(err) {
